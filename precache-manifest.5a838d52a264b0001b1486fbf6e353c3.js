9f32c4ceb6dfc832b8f8d8112298e37ff4ffef10
RagnnnaCode <filigreepvp@mail.ru>
2020-03-17 01:36:17 +0300

    Updates

---
+++ precache-manifest.5a838d52a264b0001b1486fbf6e353c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b15eee17410b6529a85d5e8b9e0e27",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "e2d6e5c8bf9cd209575a",
+    "url": "/react-app/static/css/main.e91c7b95.chunk.css"
+  },
+  {
+    "revision": "6bf7eee5153a113ad92a",
+    "url": "/react-app/static/js/2.1977c2fc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-app/static/js/2.1977c2fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d6e5c8bf9cd209575a",
+    "url": "/react-app/static/js/main.19faef0c.chunk.js"
+  },
+  {
+    "revision": "8d0c7bd2c247cbf66961",
+    "url": "/react-app/static/js/runtime-main.a86a25de.js"
+  }
+]);
